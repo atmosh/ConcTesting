c89343a3ccc3c54fb0c7e562323cb0cf29697274
PuppetM <testingStuffAndThings@outlook.com>
2018-11-16 16:24:55 +0000

    After understanding outputs this has been changed to create two output folders

--- multistage/echoScriptMulti.sh
+++ multistage/echoScriptMulti.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-echo "this is an intial test echo"
+mkdir firstOut
+
+echo "this is an intial test echo" > firstOut/file1.txt
 
 
-echo "this is a second test echo"
+echo "this is a second test echo" > firstOut/file2.txt
+
+echo "this file should not be found unless placed into an output file" > file3.txt
+
+mkdir secondOut
+
+mv file3.txt secondOut/file3.txt
