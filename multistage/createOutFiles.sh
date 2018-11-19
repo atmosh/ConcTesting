717add3038f8e123c71674732772fd39ae11feeb
PuppetM <testingStuffAndThings@outlook.com>
2018-11-19 11:43:07 +0000

    changed script to have consistant file name

--- multistage/createOutFiles.sh
+++ multistage/createOutFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir firstOut
+mkdir firstOutput
 
 echo "this is an intial test echo" > firstOut/file1.txt
 
@@ -9,6 +9,13 @@ echo "this is a second test echo" > firstOut/file2.txt
 
 echo "this file should not be found unless placed into an output file" > file3.txt
 
-mkdir secondOut
+mkdir secondOutput
 
 mv file3.txt secondOut/file3.txt
+
+
+echo "file 1"
+ls firstOutput
+echo "==="
+echo "file 2"
+ls secondOutput
