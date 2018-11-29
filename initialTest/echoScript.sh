4f6a0a42c5acfe8ffe3dc2f44ccc5a3ad00bb865
atmosh <45094660+atmosh@users.noreply.github.com>
2018-11-29 11:27:04 +0000

    Update echoScript.sh

--- initialTest/echoScript.sh
+++ initialTest/echoScript.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-
 echo "entered script ok!!!!"
+
+echo "------"
+ls
+echo "------"
+
 echo "deploy with name $redisDeploymentName"
 bosh deploy -d $redisDeploymentName redisDeploymentGit/manifests/redis.yml
 
